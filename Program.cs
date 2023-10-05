384b37da7e0eef279a269786298f69b1d9841823
Yahia Ibrahim <Yahiasheriif87@gmail.com>
2023-10-05 18:51:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var order = new Order(new CashPayment());
+        order.Checkout(1000);
+        order = new Order(new CreditCardPayment());
+        order.Checkout(1000);
+    }
+    class Order
+    {
+        private readonly IPayments _payment;
+        public Order(IPayments payment)
+        {
+            _payment = payment;
+        }
+        public void Checkout(decimal amount)
+        {
+            _payment.Pay(amount);
+        }
+    }
+    interface IPayments
+    {
+        void Pay(decimal amount);
+    }
+    class CashPayment : IPayments
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Cash Payment :{Math.Round(amount):N0}");
+        }
+    }
+
+    class CreditCardPayment : IPayments
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Credit Card Payment :{Math.Round(amount):N0}");
+        }
+    }
+
+}
